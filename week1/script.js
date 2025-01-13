ab59b5fa7ff83475fb48596864fcd263ec958931
dtd217 <duydong217@gmail.com>
2025-01-13 15:33:29 +0700

    new

---
+++ week1/script.js
@@ -0,0 +1,17 @@
+var mainSection = document.getElementById("faq");
+var faqHeader = mainSection.getElementsByClassName("faq-header")
+var faqContent = mainSection.getElementsByClassName("faq-content");
+
+for (let i = 0; i < faqHeader.length; i++) {
+   faqHeader[i].addEventListener('click', function () {
+      if (faqHeader[i].classList.contains("active")) {
+         faqHeader[i].classList.remove("active")
+         faqContent[i].style.display = 'none'
+      }
+      else {
+         faqHeader[i].classList.add("active")
+         faqContent[i].style.display = ''
+      }
+   })
+}
+

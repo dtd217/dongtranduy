254b7b4177d373cc0d429708c9fd7b6e20fbcb78
dtd217 <duydong217@gmail.com>
2025-01-14 16:09:32 +0700

    first

---
+++ week3/faq-v2-script.js
@@ -0,0 +1,23 @@
+var mainSection = document.getElementById("faq");
+var faqHeader = mainSection.getElementsByClassName("faq-header")
+var faqContent = mainSection.getElementsByClassName("faq-content");
+var faqIcon = mainSection.getElementsByClassName("faq-icon")
+
+for (let i = 0; i < faqHeader.length; i++) {
+   faqHeader[i].addEventListener('click', function () {
+      if (faqHeader[i].classList.contains("active")) {
+         faqHeader[i].classList.remove("active")
+         faqContent[i].style.display = 'none'
+         faqIcon[i].innerHTML = '&#128931;'
+      }
+      else {
+         faqHeader[i].classList.add("active")
+         faqContent[i].style.display = 'block'
+         faqIcon[i].innerHTML = '&#9866;'
+      }
+   })
+}
+
+function changeIcon() {
+
+}

8338b6ee12077dfe7466d38b6232932964eacb84
dtd217 <duydong217@gmail.com>
2025-01-14 17:04:34 +0700

    second

--- week3/faq-v2-script.js
+++ week3/faq-v2-script.js
@@ -3,21 +3,39 @@ var faqHeader = mainSection.getElementsByClassName("faq-header")
 var faqContent = mainSection.getElementsByClassName("faq-content");
 var faqIcon = mainSection.getElementsByClassName("faq-icon")
 
+// for (let i = 0; i < faqHeader.length; i++) {
+//    faqHeader[i].addEventListener('click', function () {
+//       if (faqHeader[i].classList.contains("active")) {
+//          faqHeader[i].classList.remove("active")
+//          faqContent[i].style.display = 'none'
+//          faqIcon[i].innerHTML = '&#128931;'
+//       }
+//       else {
+//          faqHeader[i].classList.add("active")
+//          faqContent[i].style.display = 'block'
+//          faqIcon[i].innerHTML = '&#9866;'
+//       }
+//    })
+// }
+
 for (let i = 0; i < faqHeader.length; i++) {
    faqHeader[i].addEventListener('click', function () {
+      for (let j = 0; j < faqHeader.length; j++) {
+         if (j !== i) {
+            faqHeader[j].classList.remove("active");
+            faqContent[j].style.display = 'none';
+            faqIcon[j].innerHTML = '&#128931;';
+         }
+      }
       if (faqHeader[i].classList.contains("active")) {
-         faqHeader[i].classList.remove("active")
-         faqContent[i].style.display = 'none'
-         faqIcon[i].innerHTML = '&#128931;'
+         faqHeader[i].classList.remove("active");
+         faqContent[i].style.display = 'none';
+         faqIcon[i].innerHTML = '&#128931;';
       }
       else {
-         faqHeader[i].classList.add("active")
-         faqContent[i].style.display = 'block'
-         faqIcon[i].innerHTML = '&#9866;'
+         faqHeader[i].classList.add("active");
+         faqContent[i].style.display = 'block';
+         faqIcon[i].innerHTML = '&#9866;';
       }
-   })
-}
-
-function changeIcon() {
-
+   });
 }
